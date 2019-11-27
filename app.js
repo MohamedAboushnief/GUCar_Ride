0c28ed93224ebdbce1ca7038c685a9ee0cc977da
MohamedAboushnief <mohamed.shenif@gmail.com>
2019-11-27 22:48:08 +0200

    passenger request controllers and model

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const user = require('./routes/user');
 const request = require('./routes/request');
 const driver_car = require('./routes/driver_car');
 const trip = require('./routes/trip');
+const passenger_request = require('./routes/passenger_request');
 var passport = require('passport');
 const port = 3000;
 
@@ -13,7 +14,7 @@ Sequelize.authenticate()
 	.then(() => {
 		console.log('database connected');
 	})
-	.catch(error => {
+	.catch((error) => {
 		console.log(error);
 	});
 
@@ -22,7 +23,7 @@ const eraseDatabaseOnSync = false;
 Sequelize.sync({ force: eraseDatabaseOnSync })
 	.then(() => console.log('Synced models with database .'))
 	.then(() => {})
-	.catch(error => console.log('Could not sync models with database .', error));
+	.catch((error) => console.log('Could not sync models with database .', error));
 
 app.use(express.json());
 app.use(passport.initialize());
@@ -30,5 +31,6 @@ app.use('/routes/users', user);
 app.use('/routes/requests', request);
 app.use('/routes/drivers_cars', driver_car);
 app.use('/routes/trips', trip);
+app.use('/routes/passenger_request', passenger_request);
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
